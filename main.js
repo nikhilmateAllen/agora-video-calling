b1222e6c0130ef1107885eff43afaadf570dcae9
Nikhil Mate <nikhil.mate@allen.ac.in>
2023-07-31 10:05:19 +0530

    changes

--- main.js
+++ main.js
@@ -175,13 +175,13 @@ function sendDataToMixPanel (){
     let localVideoStats = agoraEngine.getLocalVideoStats();
     let remoteAudioStats = agoraEngine.getLocalVideoStats();
     let remoteVideoStats = agoraEngine.getRemoteVideoStats();
-    sendEvent('HOST_AUDIO_STATS', {...localAudioStats, user: options.uid});
-    sendEvent('HOST_VIDEO_STATS', {...localVideoStats, user: options.uid});
-    sendEvent('HOST_AV_STATS', {...agoraEngine.getRTCStats(), user: options.uid, netowrk: agoraEngine.getRemoteNetworkQuality()});
+    sendEvent('VC_HOST_AUDIO_STATS', {...localAudioStats, user: options.uid});
+    sendEvent('VC_HOST_VIDEO_STATS', {...localVideoStats, user: options.uid});
+    sendEvent('VC_HOST_AV_STATS', {...agoraEngine.getRTCStats(), user: options.uid, netowrk: agoraEngine.getRemoteNetworkQuality()});
     // sendEvent('remote video stats', remoteVideoStats);
     // sendEvent('remote audio stats', remoteAudioStats);
     agoraEngine.on("exception", function(evt) {
-      sendEvent('EXCEPTION', {code: evt.code, msg: evt.msg, uid: evt.uid})
+      sendEvent('VC_EXCEPTION', {code: evt.code, msg: evt.msg, uid: evt.uid})
       // console.log(evt.code, evt.msg, evt.uid);
     })
   }, 5000);
